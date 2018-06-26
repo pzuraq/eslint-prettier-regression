e1549932a19d021eecf7b7bb4d70af5dcc9fcbe8
Christopher Garrett <me@pzuraq.com>
2018-06-26 13:47:53 -0700

    demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,19 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-    'ember'
+    'ember',
+    'prettier',
   ],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
   ],
   env: {
     browser: true
   },
   rules: {
+    // Prettier plugin
+    'prettier/prettier': 'error',
   },
   overrides: [
     // node files
